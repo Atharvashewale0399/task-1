b7a2d14b47a658baa28b9dd1658942126e039e89
shrey <shrey@DESKTOP-K047EF7>
2023-07-20 20:03:39 +0530

    static

---
+++ src/staticvariable/statickeyword.java
@@ -0,0 +1,13 @@
+//write a java program to get the cube of given number using static 
+package staticvariable;
+
+public class statickeyword {
+
+	public static void main(String[] args) {
+		int n = 6;
+		int cube = n*n*n;
+		System.out.println("the cube of 6 is :" +cube);
+
+	}
+
+}
